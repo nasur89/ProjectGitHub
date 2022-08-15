620a5f1d807bf76eb67ed71da41f8f4eb9474f8c
GrajaHUB19 <skylyraja@gmail.com>
2022-08-15 11:15:08 +0530

    two method added in emp class

--- src/test/java/com/getclass/Emp.java
+++ src/test/java/com/getclass/Emp.java
@@ -1,5 +1,17 @@
 package com.getclass;
 
 public class Emp {
+	
+	
+
+	private static void method1() {
+		System.out.println("3");
+		
+	}
+	
+	private void method2() {
+		System.out.println("Annai Kanchi PG");
+
+	}
 
 }
